fcfbaf5c5a727f9cc85ce4ab9921db730da83189
Faizyab Khalid <129151546+Faiz510@users.noreply.github.com>
2023-11-20 16:49:00 +0500

    Navbar created

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,10 +3,12 @@ import Welcome from "./components/Pages/Welcome";
 import Sigin from "./components/Pages/Sigin";
 import Signout from "./components/Pages/Signout";
 import Home from "./components/Pages/Home";
+import Navbar from "./components/layout/Navbar";
 
 const App = () => {
   return (
     <BrowserRouter>
+      <Navbar />
       <Routes>
         <Route path="/">
           <Route path="" element={<Home />}></Route>
---
+++ client/src/components/layout/Navbar.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+
+const Navbar = () => {
+  return (
+    <nav className="flex justify-between items-center bg-slate-600 text-white gap-4 px-2 md:px-10 py-2">
+      <div className="text-2xl font-semibold tracking-wider">
+        <span className="text-slate-300">Notes</span>
+        <span className="text-slate-100">App</span>
+      </div>
+
+      <ul className="flex gap-2">
+        <li>
+          <NavLink to={"/"}> Home </NavLink>
+        </li>
+        <li>
+          <NavLink to={"/signin"}> Sigin </NavLink>
+        </li>
+        <li>
+          <NavLink to={"/signout"}> Sigout </NavLink>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
