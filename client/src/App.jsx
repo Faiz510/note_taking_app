8f4dbff0460e36003a18a004168e323bef9b73c3
Faizyab Khalid <129151546+Faiz510@users.noreply.github.com>
2023-11-20 16:32:20 +0500

    intail frontend page has been created

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,22 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Welcome from "./components/Pages/Welcome";
+import Sigin from "./components/Pages/Sigin";
+import Signout from "./components/Pages/Signout";
+import Home from "./components/Pages/Home";
+
 const App = () => {
-  return <div className="text-4xl text-red-700">App</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/">
+          <Route path="" element={<Home />}></Route>
+          <Route path="/welcome" element={<Welcome />}></Route>
+          <Route path="/signin" element={<Sigin />}></Route>
+          <Route path="/signin" element={<Signout />}></Route>
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
 };
 
 export default App;
