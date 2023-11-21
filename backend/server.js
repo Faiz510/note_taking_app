8ad3bd28d38064f92353c367540ce1749bc273fa
Faizyab Khalid <129151546+Faiz510@users.noreply.github.com>
2023-11-21 16:00:21 +0500

    backend connect to database express connected

---
+++ backend/server.js
@@ -0,0 +1,14 @@
+import app from "./api.js";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+
+dotenv.config();
+
+const port = process.env.PORT;
+const DB = process.env.DB;
+
+mongoose.connect(DB).then(() => console.log("app connected to database"));
+
+app.listen(port, () => {
+  console.log(`app is listen to port ${port}`);
+});
