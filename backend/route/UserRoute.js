0808e4f03b3f1033894812e02cb81432eac6f97e
Faizyab Khalid <129151546+Faiz510@users.noreply.github.com>
2023-11-22 15:21:43 +0500

    signup handler and route created

--- backend/route/UserRoute.js
+++ backend/route/UserRoute.js
@@ -1,5 +1,9 @@
 import express from "express";
+import { signup } from "../controller/authController";
 
 const router = express.Router();
+////////////////
+
+router.route("/").post(signup);
 
 export default router;
