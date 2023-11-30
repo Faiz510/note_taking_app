6fa63fafd78c96165ca7297dbb282cfed66ea338
Faizyab Khalid <129151546+Faiz510@users.noreply.github.com>
2023-11-30 17:43:58 +0500

    category updated crud handler created modal and route updated

--- backend/controller/CategoryController.js
+++ backend/controller/CategoryController.js
@@ -7,6 +7,7 @@ import ErrorHandlerFunction from "../utilis/ErrorHandlerFunction.js";
 import Category from "../modal/CategoryModal.js";
 
 export const createCategory = catchAsync(async (req, res, next) => {
+  const { token } = req.body;
   const category = await Category.create(req.body);
 
   const user = await User.findById(req.user.id);
@@ -21,5 +22,65 @@ export const createCategory = catchAsync(async (req, res, next) => {
   user.categories.push(category);
   await user.save({ validateBeforeSave: false });
 
-  sendSucessResponse(res, user);
+  sendSucessResponse(res, user, token);
+});
+
+export const getAllCategory = catchAsync(async (req, res, next) => {
+  const user = await User.findById(req.user.id);
+
+  const category = await user.categories;
+
+  res.status(200).json({
+    status: "sucess",
+    categoryNum: category.length,
+    category,
+  });
+});
+
+export const categoryById = catchAsync(async (req, res, next) => {
+  const category = await Category.findById(req.params.id);
+
+  res.status(200).json({
+    status: "sucess",
+    category,
+  });
+});
+
+export const delCategory = catchAsync(async (req, res, next) => {
+  const { token } = req.body;
+  const category = await Category.findByIdAndDelete(req.params.id);
+
+  ErrorHandlerFunction(category, next, 400, "category not found with this id");
+
+  const user = await User.findById(req.user.id);
+
+  ErrorHandlerFunction(
+    user,
+    next,
+    400,
+    "user not logged in or user not found with id "
+  );
+
+  sendSucessResponse(res, user, token);
+});
+
+export const updateCategory = catchAsync(async (req, res, next) => {
+  const { token } = req.body;
+  const category = await Category.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+    runValidators: true,
+  });
+
+  ErrorHandlerFunction(category, next, 400, "category not found with this id");
+
+  const user = await User.findById(req.user.id);
+
+  ErrorHandlerFunction(
+    user,
+    next,
+    400,
+    "user not logged in or user not found with id "
+  );
+
+  sendSucessResponse(res, user, token);
 });
--- backend/route/CategoryRoute.js
+++ backend/route/CategoryRoute.js
@@ -1,10 +1,21 @@
 import express from "express";
 
 import { protect } from "../controller/authController.js";
-import { createCategory } from "../controller/categoryController.js";
+import {
+  categoryById,
+  createCategory,
+  getAllCategory,
+  delCategory,
+  updateCategory,
+} from "../controller/categoryController.js";
 
 const router = express.Router();
 
-router.route("/").post(protect, createCategory);
+router.route("/").post(protect, createCategory).get(protect, getAllCategory);
 
+router
+  .route("/:id")
+  .get(protect, categoryById)
+  .delete(protect, delCategory)
+  .put(protect, updateCategory);
 export default router;
