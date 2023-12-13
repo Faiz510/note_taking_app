09128320df2f24443be152ce8097e7ebab3e6979
Faizyab Khalid <129151546+Faiz510@users.noreply.github.com>
2023-12-13 11:28:21 +0500

    profileimg added to navbar

--- client/src/components/layout/Navbar.jsx
+++ client/src/components/layout/Navbar.jsx
@@ -5,6 +5,9 @@ import { useSelector } from "react-redux";
 const Navbar = () => {
   const currentUser = useSelector((state) => state.user.user.currentUser);
 
+  const baseurl = "http://localhost:3000/img/";
+  const ImgUrl = `${baseurl}${currentUser?.user?.photo}`;
+
   return (
     <nav className="flex justify-between items-center bg-slate-600 text-white gap-4 px-2 md:px-10 py-2">
       <div className="text-2xl font-semibold tracking-wider">
@@ -17,7 +20,9 @@ const Navbar = () => {
         </li>
         {currentUser ? (
           <li>
-            <NavLink to={"/profile"}> profile </NavLink>
+            <NavLink to={"/profile"}>
+              <img src={ImgUrl} className="w-8 h-8 rounded-full object-cover" />
+            </NavLink>
           </li>
         ) : (
           <li>
