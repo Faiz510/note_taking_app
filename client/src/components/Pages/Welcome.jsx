286f9299e18cf657cd35d70b1e88e88b4cdf04b8
Faizyab Khalid <129151546+Faiz510@users.noreply.github.com>
2024-04-07 19:52:49 +0500

    welcome page UI enhanced

--- client/src/components/Pages/Welcome.jsx
+++ client/src/components/Pages/Welcome.jsx
@@ -1,16 +1,20 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import noteImg from "../../assets/WellcomeNote.jpg";
 
 const Welcome = () => {
   return (
-    <section className="flex justify-center items-center h-[98vh] bg-gray-400 text-white">
+    <section
+      className={`flex justify-center items-center bg-cover bg-center h-[98vh] text-white`}
+      style={{ backgroundImage: `url('${noteImg}')` }}
+    >
       <div className="text-center">
-        <h2 className="text-2xl">Welcome</h2>
+        <h2 className="text-6xl tracking-wider">Welcome</h2>
 
-        <p> singup Up and create notes which is important </p>
+        <p className="text-2xl tracking-wider"> singup Up and create notes </p>
 
         <button className="">
-          <Link to={"/signup"} className="bg-white px-2 text-black mt-2">
+          <Link to={"/signup"} className="bg-white px-4 py-1 mt-5 text-black ">
             Join in
           </Link>
         </button>
